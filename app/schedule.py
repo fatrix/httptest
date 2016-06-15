85179a9e3b7afa454ee92d6c8d15e1fc5f9c485d
Philip Sahli <philip@sahli.net>
2016-06-15 04:48:34 -0400

    fix datetime keyerror

--- app/schedule.py
+++ app/schedule.py
@@ -13,6 +13,7 @@ def func(self):
         result = json.loads(r.json()['returned']['content'])
         results.append(result)
         result_counters = result['message'][1]
+        datetime = result['message'][2]
 
         failure_count_after = result_counters['failures'] + result_counters['errors']
 
@@ -22,11 +23,11 @@ def func(self):
         self.info(self.rid, "Diff %s to %s" % (failure_count_before, failure_count_after))
         if failure_count_before < failure_count_after:
             send_alarm=True
-            subject = "HTTPTest - Test '%s' failed on %s" % (test.data['name'], test.data['datetime'])
+            subject = "HTTPTest - Test '%s' failed on %s" % (test.data['name'], datetime)
             msg = "Test %s failed" % (test.data['name'])
         elif failure_count_after == 0 and failure_count_before is not failure_count_after:
             send_recover=True
-            subject = "HTTPTest - Test '%s' recovered on %s" % (test.data['name'], test.data['datetime'])
+            subject = "HTTPTest - Test '%s' recovered on %s" % (test.data['name'], datetime)
             msg = "Test %s recovered" % (test.data['name'])
 
         self.info(self.rid, "%s %s" % (str(send_alarm), str(send_recover)))
