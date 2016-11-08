a83135cff38add0701f2e980b8469e1fa175b345
Philip Sahli <philip@sahli.net>
2016-11-08 11:29:55 +0100

    update schedule for ssl fix

--- app/schedule.py
+++ app/schedule.py
@@ -2,12 +2,13 @@ def func(self):
     import requests
     import json
     import utils
-
+    import copy
     import time
 
     tests = self.datastore.filter("schedule", "yes")
 
     results = []
+    alarms = []
 
     self.info(self.rid, "Starting for %s tests" % len(tests))
 
@@ -42,11 +43,11 @@ def func(self):
 
             # TEST
             if failure_count_before < failure_count_after:
-                send_alarm=True
+                send_alarm = True
                 subject = "HTTPTest - Test '%s' failed" % test.data['name']
                 msg = "Test %s failed" % (test.data['name'])
             elif failure_count_after == 0 and failure_count_before is not failure_count_after:
-                send_recover=True
+                send_recover = True
                 subject = "HTTPTest - Test '%s' recovered" % test.data['name']
                 msg = "Test %s recovered" % (test.data['name'])
 
@@ -59,22 +60,43 @@ def func(self):
                     self.error(self.rid, "Exception from utils.send_report")
 
             # SSL
+            try:
+                ssl_alarm = copy.deepcopy(test.data['ssl_alarm'])
+            except:
+                self.info(self.rid, "Create empty dict test.data['ssl_alarm']")
+                ssl_alarm = {}
+
             for env, info in ssl_info.items():
                 if not type(info) is dict:
                     self.warn(self.rid, "Not a dict (%s)" % str(info))
                     continue
-                if not test.data.get('ssl_alarm', None):
-                    test.data['ssl_alarm'] = {}
-                if not test.data['ssl_alarm'].get('%s' % info['serialNumber'], None):
-                    test.data['ssl_alarm'].clear()
-                    test.data['ssl_alarm']['%s' % info['serialNumber']] = {}
-                for left in [2,  5,  10, 30]:
-                    leftAlarmed = test.data['ssl_alarm']['%s' % info['serialNumber']].get(str(left), False)
-                    if info['daysLeft'] == left and not leftAlarmed:
-                        test.data['ssl_alarm']['%s' % info['serialNumber']][left] = True
+                self.info(self.rid, env+" ssl_info: "+str(info))
+                self.info(self.rid, "Checking for resulted serialNumber "+info['serialNumber'])
+                self.info(self.rid, "ssl_alarm from store at begin: %s" % str(test.data['ssl_alarm']))
+
+                if not ssl_alarm.get(env, None):
+                    ssl_alarm[env] = {}
+
+                if not ssl_alarm[env].get('%s' % info['serialNumber'], None):
+                    self.info(self.rid, "Cleanup test.data['ssl_alarm'] and create empty dict for serialNumber")
+                    ssl_alarm[env].clear()
+                    ssl_alarm[env]['%s' % info['serialNumber']] = {}
+                self.info(self.rid, "ssl_alarm3: %s" % str(ssl_alarm[env]))
+
+                for left in [2,  5,  6, 10, 30, 65]:
+                    is_alarmed = ssl_alarm[env]['%s' % info['serialNumber']].get(str(left), False)
+                    self.info(self.rid, "Left: %s: %s" % (left, is_alarmed))
+                    if info['daysLeft'] == left and not is_alarmed:
+                        self.info(self.rid, "must alert for ssl expiration, %s | %s | %s | %s " % (left, is_alarmed, ssl_alarm[env], info['commonName']))
+                        ssl_alarm[env]['%s' % info['serialNumber']][str(left)] = True
+                        self.info(self.rid, "must alert for ssl expiration, %s | %s | %s | %s | is_alarmed: %s" % (left, is_alarmed, ssl_alarm[env], info['commonName'], ssl_alarm[env]['%s' % info['serialNumber']][str(left)]))
+                        self.info(self.rid, "ssl_alarm after: %s" % ssl_alarm[env])
 
                         subject = "HTTPTest - Test '%s': The SSL certificate on environment '%s' will expire in %s days!" % (test.data['name'], env, info['daysLeft'])
                         utils.send_report(self, test.data['testid'], test.data['email'], test.data['name'], run=mydatetime, subject=subject)
+
+                        #self.datastore.update(test)
+                        #self.datastore.session.commit()
 
         except Exception, e:
             import sys, os
@@ -88,11 +110,17 @@ def func(self):
             self.datastore.session.rollback()
 
         finally:
-            self.info(self.rid, "Finally: %s" % test)
+            self.info(self.rid, "Finally: %s" % test.data['testid'])
+            test.data['ssl_alarm'] = ssl_alarm
+            self.info(self.rid, "ssl_alarm after2: %s" % test.data['ssl_alarm'])
+
+            alarms.append(ssl_alarm)
             try:
-                self.info(self.rid, "Lasts: %s" % test.data['last'].datetime)
-            except:
-                pass
+                #self.info(self.rid, "Lasts: %s" % test.data['last'].datetime)
+                self.info(self.rid, "Alarm: %s" % str(test.data['ssl_alarm']))
+            except Exception, e:
+                print e
+                self.error(self.rid, e.message)
             self.datastore.update(test)
             self.datastore.session.commit()
 
@@ -102,4 +130,4 @@ def func(self):
 
     self.info(self.rid, "Ended for %s tests" % len(tests))
 
-    return results
+    return results, alarms
