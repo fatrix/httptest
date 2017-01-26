21e1095a5239a43adc56579b61c95e499381265a
Philip Sahli <philip@sahli.net>
2017-01-26 07:54:34 +0100

    use tumbo.io in test

--- app/httptest.py
+++ app/httptest.py
@@ -8,7 +8,7 @@ def func(self, data, version, response_obj=None):
     >>> self.warn = lambda x, y: x
     >>> self.debug = lambda x, y: x
     >>> self.rid = 1234
-    >>> data = {'tests': [{'name': 'test_home', 'asserts': {'assert_status_code_is': 200}, 'uri': '/'}, {'name': 'test_fails', 'asserts': {'assert_status_code_is': 500}, 'uri': '/'}], 'environments': [{'name': 'sahli', 'base_url': 'https://sahli.net'}, {'name': 'httptest', 'base_url': 'https://httptest.sahli.net'}]}
+    >>> data = {'tests': [{'name': 'test_home', 'asserts': {'assert_status_code_is': 200}, 'uri': '/'}, {'name': 'test_fails', 'asserts': {'assert_status_code_is': 500}, 'uri': '/'}], 'environments': [{'name': 'sahli', 'base_url': 'https://sahli.net'}, {'name': 'httptest', 'base_url': 'https://tumbo.io/'}]}
     >>> version = 2
     >>> results, ssl_info, total_counter, success =  func(self, data, version, True)
     >>> results.keys()
