841de1f9efcd7e07d82ac4dd8858f9d34d873ba2
user name <user@domain.com>
2018-08-18 21:28:33 +0200

    fix entrypoint import

--- entrypoint.py
+++ entrypoint.py
@@ -1,14 +1,5 @@
 def func(self):
-	import sys, os
-	import py_compile
-	if  os.environ['EXECUTOR'] != "docker":
-		sys.path.append("/home/philipsahli/workspace/httptest/app")
-		py_compile.compile("/home/philipsahli/workspace/httptest/app/httptest.py", doraise=True)
-		import entrypoint, httptest
-		reload(entrypoint)
-		reload(httptest)
-	else:
-		from app import entrypoint
-		from app import httptest
-		print httptest
+	import sys 
+	sys.path.append("/home/tumbo/.virtualenvs/tumbo/src/httptest")
+	from app import entrypoint
 	return entrypoint.func(self)
