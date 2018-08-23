813f64ae030ef449bc27d4713abf878635d64d98
Philip Sahli <philip@sahli.net>
2018-08-23 09:52:52 +0200

    cleanup install_module

--- install_module.py
+++ install_module.py
@@ -1,10 +1,6 @@
 def func(self, r=""):
 	import os, sys
 	module = "-e " + self.settings.GIT_URL
-	try:
-		raise Exception()
-		import cloud; print cloud
-	except Exception:
-		pip = os.path.join(os.path.dirname(sys.executable), "pip")
-		r=os.popen("%s install %s" % (pip, module)).read()
+	pip = os.path.join(os.path.dirname(sys.executable), "pip")
+	r=os.popen("%s install %s" % (pip, module)).read()
 	return r
