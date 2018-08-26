b362abdba231215468336a64aa9c5ebc9d98e245
Philip Sahli <philip@sahli.net>
2018-08-26 14:26:36 +0200

    remove unused functions

--- get_identity.py
+++
@@ -1,4 +0,0 @@
-def func(self):
-    from core.responses import JSONResponse
-    import json
-    return JSONResponse(json.dumps(self.identity))
--- get_one1.py
+++
@@ -1,3 +0,0 @@
-def func(self):
-    import json
-    return self.responses.JSONResponse(json.dumps(self.datastore.get("name", "My Test", lock=False, nowait=True).data['runs'][0].keys()))
