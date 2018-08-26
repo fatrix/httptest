77adc0e9ccbe68985e0ee956c41b108acad21472
Philip Sahli <philip@sahli.net>
2018-08-26 14:34:04 +0200

    remove unused functions

--- debug_response.py
+++
@@ -1,6 +0,0 @@
-def func(self):
-	import json
-	r={'request': {'Connection': 'keep-alive', 'Accept-Encoding': 'gzip, deflate', 'Accept': '*/*', 'User-Agent': 'python-requests/2.9.1'}, 'response': {'Content-Encoding': 'gzip', 'Set-Cookie': 'session_id=5d4e427a139bed2d9783aed06ab872d73d5a0777; Expires=Tue, 19-Jul-2016 07:18:33 GMT; Max-Age=7776000; Path=/', 'Server': 'Cherokee', 'Connection': 'close', 'Date': 'Wed, 20 Apr 2016 07:18:33 GMT', 'Content-Type': 'text/html; charset=utf-8'}}
-	self.datastore.write_dict(r)
-	self.info(self.rid, "stored")
-	return self.responses.JSONResponse(json.dumps(r))
--- empty_store.py
+++
@@ -1,3 +0,0 @@
-def func(self):
-	for row in self.datastore.all():
-		self.datastore.delete(row)
