3c20808bd5fec94d4809cb68f61a29e66e2fb726
Philip Sahli <philip@sahli.net>
2018-08-26 15:43:38 +0200

    fix intend

--- install_module.py
+++ install_module.py
@@ -3,5 +3,5 @@ def func(self, r=""):
 	module = "-e " + self.settings.GIT_URL
 	pip = os.path.join(os.path.dirname(sys.executable), "pip")
 	r=os.popen("%s install %s" % (pip, module)).read()
-    os.kill(1, signal.SIGHUP)
+	os.kill(1, signal.SIGHUP)
 	return r
