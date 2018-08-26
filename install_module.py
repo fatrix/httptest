701e6c8d30f4996518594e92e2e3917c6e4a4a2c
Philip Sahli <philip@sahli.net>
2018-08-26 07:58:53 +0200

    add signal.SIGHUP

--- install_module.py
+++ install_module.py
@@ -1,6 +1,7 @@
 def func(self, r=""):
-	import os, sys
+	import os, sys,signal
 	module = "-e " + self.settings.GIT_URL
 	pip = os.path.join(os.path.dirname(sys.executable), "pip")
 	r=os.popen("%s install %s" % (pip, module)).read()
+    os.kill(1, signal.SIGHUP)
 	return r
